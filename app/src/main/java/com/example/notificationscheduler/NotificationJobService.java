d46f01110db60737cc73c22cc9f2fb20ce39cd95
Ashwani Kumar <akumar@monmouthcollege.edu>
2019-02-13 21:17:21 -0600

    implements on OnstartJob

--- app/src/main/java/com/example/notificationscheduler/NotificationJobService.java
+++ app/src/main/java/com/example/notificationscheduler/NotificationJobService.java
@@ -1,16 +1,64 @@
 package com.example.notificationscheduler;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Intent;
+import android.graphics.Color;
 
 public class NotificationJobService extends JobService {
+    NotificationManager mNotifyManager;
+    // Notification channel id
+    private static final String PRIMARY_CHANNEL_ID =
+            "primary_notification_channel";
     @Override
     public boolean onStartJob(JobParameters params) {
+
+        //Create the notification channel
+        createNotificationChannel();
+
+//Set up the notification content intent to launch the app when clicked
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (this, 0, new Intent(this,
+                        MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+
         return false;
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
         return false;
+    }
+
+    /**
+     * Creates a Notification channel, for OREO and higher.
+     */
+    public void createNotificationChannel() {
+
+        // Define notification manager object.
+        mNotifyManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        // Notification channels are only available in OREO and higher.
+        // So, add a check on SDK version.
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+
+            // Create the NotificationChannel with all the parameters.
+            NotificationChannel notificationChannel = new NotificationChannel
+                    (PRIMARY_CHANNEL_ID,
+                            "Job Service notification",
+                            NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription
+                    ("Notifications from Job Service");
+
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
     }
 }
