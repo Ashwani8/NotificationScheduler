75fe0a62290fac13e304136baa6d49b1f7532db3
Ashwani Kumar <akumar@monmouthcollege.edu>
2019-02-15 17:36:11 -0600

    implements a JobService

--- app/src/main/java/com/example/notificationscheduler/NotificationJobService.java
+++ app/src/main/java/com/example/notificationscheduler/NotificationJobService.java
@@ -8,13 +8,15 @@ import android.app.job.JobService;
 import android.content.Intent;
 import android.graphics.Color;
 
+import androidx.core.app.NotificationCompat;
+
 public class NotificationJobService extends JobService {
     NotificationManager mNotifyManager;
     // Notification channel id
     private static final String PRIMARY_CHANNEL_ID =
             "primary_notification_channel";
     @Override
-    public boolean onStartJob(JobParameters params) {
+    public boolean onStartJob(JobParameters jobParameters) {
 
         //Create the notification channel
         createNotificationChannel();
@@ -24,12 +26,22 @@ public class NotificationJobService extends JobService {
                 (this, 0, new Intent(this,
                         MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
 
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder( this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("Job Service")
+                .setContentText("Your job ran to completions!")
+                .setContentIntent(contentPendingIntent)
+                .setSmallIcon(R.drawable.ic_job_running)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setAutoCancel(true);
+        mNotifyManager.notify(0,builder.build());
         return false;
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
-        return false;
+        return true;
     }
 
     /**
